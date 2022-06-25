6d13f72eaeb36edc0bb2abc96f7351cdc6917978
scherepanov <sergey@cherepanov.org.ua>
2022-06-26 00:38:22 +0400

    Change BLOCK_QUARTZ_GLASS recipe

--- src/main/java/paulevs/betternether/recipes/RecipeRegister.java
+++ src/main/java/paulevs/betternether/recipes/RecipeRegister.java
@@ -1,6 +1,7 @@
 package paulevs.betternether.recipes;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -12,15 +13,15 @@ public class RecipeRegister
 	public static void register()
 	{
 		addRecipe(BlocksRegister.BLOCK_INK_BUSH_SEED, new ItemStack(Items.DYE, 3));
-		addRecipe(BlocksRegister.BLOCK_CINCINNASITE, BlocksRegister.BLOCK_CINCINNASITE_FORGED);
-		addRecipe(Items.QUARTZ, BlocksRegister.BLOCK_QUARTZ_GLASS);
+		addRecipe(BlocksRegister.BLOCK_CINCINNASITE, BlocksRegister.BLOCK_CINCINNASITE_FORGED, 1);
+		addRecipe(Blocks.QUARTZ_BLOCK, BlocksRegister.BLOCK_QUARTZ_GLASS, 4);
 	}
 	
-	private static void addRecipe(Block material, Block result)
+	private static void addRecipe(Block material, Block result, int amount)
 	{
 		if (material != null && result != null)
 		{
-			FurnaceRecipes.instance().addSmelting(Item.getItemFromBlock(material), new ItemStack(result), 0);
+			FurnaceRecipes.instance().addSmelting(Item.getItemFromBlock(material), new ItemStack(result, amount), 0);
 		}
 	}
 	
